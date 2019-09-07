106baebbddd92a21b1b2c6027c61b38dd5cc744f
zack <tahizak@gmail.com>
2019-09-07 13:02:59 +0200

    fix conda install2

--- conda/conda_install.sh
+++ conda/conda_install.sh
@@ -7,7 +7,7 @@ echo "install conda"
 #TO DO: force all command yes
 apt-get install bzip2 libxml2-dev
 wget https://repo.anaconda.com/archive/Anaconda2-2019.07-Linux-x86_64.sh
-bash Anaconda2-2019.07-Linux-x86_64.sh && rm $_
+bash Anaconda2-2019.07-Linux-x86_64.sh -b && rm $_
 source .bahsrc
 exit 2
 #gcloud compute instances stop $instance 
