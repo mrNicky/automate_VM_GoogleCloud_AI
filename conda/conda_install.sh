a08b0c40ac520f3b5133eca075e054a859474b94
zack <tahizak@gmail.com>
2019-09-07 12:39:37 +0200

    conda intall 2

--- conda/conda_install.sh
+++ conda/conda_install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 #Set up VM with Anaconda
-sudo -s
 
 #apt-get update
 echo "install conda"
 #Install Anaconda
 #TO DO: force all command yes
-apt-get install bzip2 libxml2-dev
-wget https://repo.anaconda.com/archive/Anaconda2-2019.07-Linux-x86_64.sh
-bash Anaconda2-2019.07-Linux-x86_64.sh && rm $_
+sudo apt-get install bzip2 libxml2-dev
+sudo wget https://repo.anaconda.com/archive/Anaconda2-2019.07-Linux-x86_64.sh
+sudo bash Anaconda2-2019.07-Linux-x86_64.sh && rm $_
 source .bahsrc
 exit 2
-gcloud compute instances stop $instance 
+#gcloud compute instances stop $instance 
