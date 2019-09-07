c35d0d94d6de593210c8f2445094b72a6a14a585
zack <tahizak@gmail.com>
2019-09-07 13:04:50 +0200

    fix conda install2

--- conda/conda_install.sh
+++ conda/conda_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Set up VM with Anaconda
-#apt-get update
+apt-get -y update
 echo "install conda"
 #Install Anaconda
 #TO DO: force all command yes
-apt-get install bzip2 libxml2-dev
+apt-get -y install bzip2 libxml2-dev
 wget https://repo.anaconda.com/archive/Anaconda2-2019.07-Linux-x86_64.sh
 bash Anaconda2-2019.07-Linux-x86_64.sh -b && rm $_
 source .bahsrc
