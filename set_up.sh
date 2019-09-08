2fa37eca71096ce2ca4afae65c0df37290aecf4f
zack <tahizak@gmail.com>
2019-09-08 23:12:21 +0200

    add GPU 3

--- set_up.sh
+++ set_up.sh
@@ -6,8 +6,7 @@ install_conda () {
 	gcloud compute instances start $2 --zone europe-west1-b
         gcloud compute ssh --project $1 --zone europe-west1-b $2 --command="wget -O - \
         https://raw.githubusercontent.com/mrNicky/automate_VM_GoogleCloud_AI/master/conda/conda_install.sh \
-        | sudo bash && source ~/.bashrc \
-	&& conda install --file"
+        | sudo bash && source ~/.bashrc"
 }
 
 start_vm () {
