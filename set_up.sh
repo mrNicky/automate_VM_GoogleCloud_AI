7c607c23d2e1107d1a70312b5e675a9d27d8ff33
zack <tahizak@gmail.com>
2019-09-08 23:07:50 +0200

    add GPU

--- set_up.sh
+++ set_up.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-#read -p 'entrer le nom de votre projet : ' projet
-#read -p 'entrer le nom de base de votre instance : ' instance
-read -p 'pour vous connecter sans recreer de vm, taper Y sinon N : ' Y
 
-#projet=${projet}silabfruit
-#instance=${instance}silabfruitins
-projet="prosilabfruit"
-#instance=${instance}"instsilabfruitins1"
-instance="gogo123gogo"
+echo Entrer votre login google cloud pour commencer : `gcloud auth login`
 
-#Connect and install Anaconda
 install_conda () {
-	echo "install conda"
-# addzone	gcloud compute instances start $2
+	gcloud compute instances start $2 --zone europe-west1-b
         gcloud compute ssh --project $1 --zone europe-west1-b $2 --command="wget -O - \
         https://raw.githubusercontent.com/mrNicky/automate_VM_GoogleCloud_AI/master/conda/conda_install.sh \
         | sudo bash && source ~/.bashrc \
@@ -20,48 +11,57 @@ install_conda () {
 }
 
 start_vm () {
-        gcloud compute instances start $2
+        gcloud compute instances start $2 --zone europe-west1-b
         gcloud compute ssh --project $1 --zone europe-west1-b $2
 }
+
+stop_vm () {
+
+	gcloud compute instances stop $1
+}
+
+
+read -p 'entrer le nom de votre projet : ' projet
+read -p 'entrer le nom de base de votre instance : ' instance
+read -p 'souhaitez-vous arrêter votre vm Y/n : ' stopvm
+read -p 'pour vous connecter sans recreer de vm, taper Y sinon N : ' Y
+
+projet=${projet}"mldeep101PROJET"
+instance=${instance}"mldeep101INSTANCE"
+
 
 if [ $Y = "Y" ]
 then 
 	gcloud config set project $projet
 	start_vm $projet $instance
+elif [ $stopvm = "Y" ]
+then
+	stop_vm $instance
+
 else
-#	read -p 'entrer le nombre de VM que vous souhaitez : ' N
-	
-	echo "install snapd"
-	#Logging and Install and update 
+	gcloud projects create $projet && gcloud config set project $_
+	#Installn billing google cloud
 	sudo apt -y install snapd
-
-	echo "install sdk"
 	sudo snap install google-cloud-sdk --classic
-	
-	echo "update gcloud"
-	#v=`gcloud version | grep -m1 -o -E '[0-9|.]+'`
-	#gcloud components update $v --quiet
-	
-	echo "creatE PROJECT ---------"
-	#Create project
-	#$1 = name project
-	#--- TO DO: check if project exist else create this project
-	#gcloud projects create $projet && gcloud config set project $_
-
 	gcloud alpha billing projects link $projet --billing-account `gcloud alpha billing accounts list | grep -o '^[A-Z0-9-]*' | tail -n1`
 	
 	#Create instance from ubuntu:1804:lts
 	#for (( i=1; i <= $N; i=++i ));do 
-	#gcloud compute instances create $instance \
-	#--image-family ubuntu-1804-lts --image-project gce-uefi-images --zone europe-west1-b
-	#instance=$instance$i
-	echo "INSTALL CONDA -------------"
+	gcloud compute instances create $instance \
+	--image-family ubuntu-1804-lts --image-project gce-uefi-images --zone europe-west1-b \
+	--machine-type n1-standard-2 --accelerator type=nvidia-tesla-k80,count=1 \ 
+	 --maintenance-policy TERMINATE --restart-on-failure \
+	 --metadata startup-script='#!/bin/bash
+    echo "Checking for CUDA and installing."
+    # Check for CUDA and try to install.
+    if ! dpkg-query -W cuda-10-0; then
+      curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+      dpkg -i ./cuda-repo-ubuntu1604_10.0.130-1_amd64.deb
+      apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+      apt-get update
+      apt-get install cuda-10-0 -y
+    fi'
+	
 	install_conda $projet $instance
-
-	echo "INSTALL LIBRAIRIES TO DO ----------"
-	
-	
-	
-	
 	start_vm $projet $instance
 	fi
