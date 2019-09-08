cfe140b1fcd7477bad90b9ff642045b2b6d1200d
zack <tahizak@gmail.com>
2019-09-08 14:01:21 +0200

    conda fix 9

--- conda/conda_install.sh
+++ conda/conda_install.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-#mkdir $HOME/.conda
+mkdir $HOME/.conda
 
 
 #Set up VM with Anaconda
-#apt-get -y update
-#echo "install conda"
+apt-get -y update
+echo "install conda"
 #Install Anaconda
 #TO DO: force all command yes
-#apt-get -y install bzip2 libxml2-dev
-#wget https://repo.continuum.io/archive/Anaconda3-2019.07-Linux-x86_64.sh -O ~/anaconda.sh
-#bash ~/anaconda.sh -b -p $HOME/anaconda
+apt-get -y install bzip2 libxml2-dev
+wget https://repo.continuum.io/archive/Anaconda3-2019.07-Linux-x86_64.sh -O ~/anaconda.sh
+bash ~/anaconda.sh -b -p $HOME/anaconda
 
 
-#export PATH=~/anaconda/bin:$PATH
-#conda init bash
-source .bashrc 
-#conda activate 
+export PATH=~/anaconda/bin:$PATH
+conda init bash
 #exit 2
 #gcloud compute instances stop $instance 
 
--- start_vm.sh
+++ start_vm.sh
@@ -3,11 +3,17 @@
 projet="prosilabfruit"
 instance="didi5didi6"
 
+
 start_vm () {
-        gcloud compute instances start $instance
-        gcloud compute ssh --project $1 --zone europe-west1-b $2
+	gcloud compute instances start $instance
+	gcloud compute ssh --project $1 --zone europe-west1-b $2 --command="wget -O - \
+	https://raw.githubusercontent.com/mrNicky/automate_VM_GoogleCloud_AI/master/conda/conda_install.sh \
+	sudo bash && source ~/.bashrc \
+	&& conda activate"
 }
 
+start_vm $project $instance
 
-start_vm $projet $instance
+
+
 
